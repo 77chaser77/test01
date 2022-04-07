d2546ee589874c241accf8d5e7ec9ad5b857baaa
77chaser77 <eddy1206@naver.com>
2022-04-07 11:56:01 +0900

    1st

---
+++ 2/js_fold/main.js
@@ -0,0 +1,36 @@
+// 현재시간 표시 기능
+document.getElementById("time").innerHTML = new Date().toLocaleDateString();
+
+function pink() {
+  document.body.style.backgroundColor = "pink";
+}
+function lightblue() {
+  document.body.style.backgroundColor = "lightblue";
+}
+function yellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+function reset() {
+  document.body.style.backgroundColor = "linen";
+}
+
+function showhtml() {
+  document.getElementById("fig").src = "img/html5.png";
+  document.getElementById("desc").innerHTML
+  = "설명"
+}
+function showcss() {
+  document.getElementById("fig").src = "img/html5.png";
+  document.getElementById("desc").innerHTML
+  = "설명"
+}
+function showjs() {
+  document.getElementById("fig").src = "img/html5.png";
+  document.getElementById("desc").innerHTML
+  = "설명"
+}
+
+function hide()
+{
+  
+}
